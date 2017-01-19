9e5babab3cb26529ed8e7fc808e45301984fbbc0
kadnox <yvann.josso@gmail.com>
2017-01-19 20:28:47 +0100

    First commit tpJPA

---
+++ tpJpa/src/main/java/domain/Personne.java
@@ -0,0 +1,83 @@
+package domain;
+
+import java.util.Collection;
+import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Personne {
+
+	private int id ;
+	private String nom;
+	private String prenom;
+	private Collection<Personne> amis;
+	private Collection<Appareil> appareils;
+	private Collection<Maison> maisons;
+
+	public Personne(){
+		
+	}
+	
+	public Personne(String nom, String prenom, Collection<Personne> amis,
+			Collection<Appareil> appareils, Collection<Maison> maisons) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.amis = amis;
+		this.appareils = appareils;
+		this.maisons = maisons;
+	}
+
+	@Id
+	@GeneratedValue
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	@ManyToMany
+	public Collection<Personne> getAmis() {
+		return amis;
+	}
+
+	public void setAmis(Collection<Personne> amis) {
+		this.amis = amis;
+	}
+	@OneToMany(mappedBy="proprio")
+	public Collection<Appareil> getAppareils() {
+		return appareils;
+	}
+
+	public void setAppareils(Collection<Appareil> appareils) {
+		this.appareils = appareils;
+	}
+	@OneToMany(mappedBy="proprio")
+	public Collection<Maison> getMaisons() {
+		return maisons;
+	}
+
+	public void setMaisons(Collection<Maison> maisons) {
+		this.maisons = maisons;
+	}
+
+}
